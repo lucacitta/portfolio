dcf571a42478ecd6c5f9fa2a7e258111a03797da
Luca Citta <lucacitta@gmail.com>
2021-08-17 22:26:07 +0200

    'VersionInglesAgregada'

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -24,6 +24,29 @@ def home(request):
             fail_silently=False,
             )
         return redirect('/?valido')
-
-
     return render(request,'index.html',{})
+
+
+def homeEn(request):
+    if request.method=='POST':
+        data=request.POST
+        nombre=data.get('name')
+        email=data.get('email')
+        asunto=data.get('subject')
+        mensaje=data.get('message')
+        email1=send_mail(
+            asunto,
+            (f'{nombre} con el email {email} accedio al formulario del portfolio y te envió el siguiente mensaje\n\n{mensaje}'),
+            EMAIL_HOST_USER,
+            ['lucacitta@gmail.com'],
+            fail_silently=False,
+            )
+        email2=send_mail(
+            'Respuesta automatica a formulario Luca Cittá Giordano',
+            (f'Hola {nombre} los mails se enviaron correctamente, voy a responder lo mas rapido posible, gracias por contactar y saludos\nLuca Citta Giordano'),
+            EMAIL_HOST_USER,
+            [email],
+            fail_silently=False,
+            )
+        return redirect('indexEn/?valido')
+    return render(request,'indexEn.html',{})
