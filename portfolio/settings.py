3aa15b419ef4ecbc0374acd61a62f99ed5a7c473
Luca Citta <lucacitta@gmail.com>
2021-10-12 12:02:43 +0200

    "Cambios_seg"

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,7 +131,10 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='lucacitta.python@gmail.com'
-EMAIL_HOST_PASSWORD='Mymamapython12'
+
+EMAIL_HOST_USER='' #EMAIL DE TU CUENTA
+EMAIL_HOST_PASSWORD='' #CONTRASEÑA DE TU CUENTA
+
+EMAIL_ADMIN=''  #EMAIL DEL ADMINISTRADOR
 
 
